115112cceb6a0536269e2731a9cc2517d29ae5ae
dave <dave@davidgwilcox.com>
2013-02-11 18:58:35 +1100

    added the tweetstream hello world. I think sinatra is overkill

--- bot.rb
+++ bot.rb
@@ -2,6 +2,18 @@ class TwitterBot < Sinatra::Base
 	register Sinatra::ConfigFile
 	config_file 'config.yml'
 
+	TweetStream.configure do |config|
+		config.consumer_key       = settings.TWITTER_CONSUMER_KEY
+		config.consumer_secret    = settings.TWITTER_CONSUMER_SECRET
+		config.oauth_token        = settings.TWITTER_ACCESS_KEY
+		config.oauth_token_secret = settings.TWITTER_ACCESS_SECRET
+		config.auth_method        = :oauth
+	end
+
+	TweetStream::Client.new.userstream do |status|
+		puts status.text
+	end
+
 	get '/hi' do
 		settings.TWITTER_CONSUMER_KEY
 	end
