57980ef55a662ed192958084a64791c3306a06ab
dave <dave@davidgwilcox.com>
2013-02-18 14:46:45 +1100

    added User#find_or_create_by_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < Sequel::Model(:users)
+	def self.find_or_create_by_twitter_user user
+		u = User.where( name: user.screen_name ).first
+		if u.nil?
+			u = User.new name: user.screen_name, score: 1
+			u.save
+		end
+		u
+	end
 end
